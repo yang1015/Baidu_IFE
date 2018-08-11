de4ac63201a026fc1555f0aa57f7926e90d7a624
yang1015 <itsak1992@gmail.com>
2018-08-11 16:33:52 +0800

    update

--- CourseTasks/Academy1/ShrewedSeller/app.js
+++ CourseTasks/Academy1/ShrewedSeller/app.js
@@ -2,26 +2,100 @@ let tableWrapperDom = document.getElementById('table-wrapper');
 let tableHeaderRow = document.getElementById('table-header');
 let tableData = document.getElementById('data-table');
 
-let regionSelected = "华东";
-let productSelected = "手机";
+let regionCheckboxDom = document.getElementsByClassName('region-checkbox')[0];
+let productCheckboxDom = document.getElementsByClassName('product-checkbox')[0];
 
-function handleRegionChange(regionChosen) {
-    emptyDataTable();
-    regionSelected = regionChosen;
-    getChosenData(regionSelected, productSelected);
+let regionSelected = [];
+let regionAllChecked = false;
+let productAllChecked = false;
+let productSelected = [];
+let regionArr = ['华北', "华南", "华东"];
+
+
+function handleClick(category, value) {
+    if (category == "region") {
+        if (value != '全选') checkIfExisted(category, value);
+        else {
+            regionAllChecked = !regionAllChecked;
+            checkAllOrEmptyAll(category, regionAllChecked); // 置反
+        }
+    } else if (category == "product") {
+        if (value != '全选') checkIfExisted(category, value);
+        else {
+            productAllChecked = !productAllChecked;
+            checkAllOrEmptyAll(category, !productAllChecked); // 置反
+        }
+    }
+    console.log(regionSelected);
+    console.log(productSelected);
+
+    getChosenData(regionSelected, productSelected)
 }
 
-function handleProductChange(productChosen) {
-    emptyDataTable();
-    productSelected = productChosen;
-    getChosenData(regionSelected, productSelected);
+function checkIfExisted(type, value) {
+    if (type == "region") {
+        if (regionSelected.length == 0) {
+            regionSelected.push(value);
+        } else {
+            for (let i = 0; i < regionSelected.length; i++) {
+                if (regionSelected[i] == value) { // 已经存在 再点是为了remove
+                    regionSelected.splice(i, 1); // i为起始位置 1为删除个数
+                    return;
+                }
+            }
+            regionSelected.push(value);
+        }
+
+    } else if (type == 'product') {
+        console.log(value);
+        if (productSelected.length == 0) {
+            productSelected.push(value);
+        } else {
+            for (let i = 0; i < productSelected.length; i++) {
+                console.log(productSelected[i]);
+                if (productSelected[i] == value) { // 已经存在 再点是为了remove
+                    productSelected.splice(i, 1); // i为起始位置 1为删除个数
+                    return; // 一定要return 不然无论如何下面都会再加一遍
+                }
+            }
+            // for loop了 没有 要添加
+            // for loop了 有 已经添加了
+            productSelected.push(value);
+        }
+    }
 }
 
-function getChosenData(regionChosen, productChosen) {
+function checkAllOrEmptyAll(category, AllChecked) {
+    let len = category == "region" ? regionCheckboxDom.childNodes.length : productCheckboxDom.childNodes.length;
+    for (let i = 0; i < len; i++) {
+        let inputDom = category == "region" ? regionCheckboxDom.childNodes[i] : productCheckboxDom.childNodes[i];
+        inputDom.checked = AllChecked ? true : false;
+        if (category == "region") {
+            if (AllChecked) regionSelected = ['华北', "华南", "华东"];
+            regionSelected = []
+        } else {
+            if (AllChecked) productSelected = ['手机', "智能音箱", "笔记本"];
+            productSelected = []
+        }
+    }
+}
+
+function getChosenData(regionSelected, productSelected) {
     let renderData = [];
     for (let i = 0; i < sourceData.length; i++) {
-        if (sourceData[i].region == regionChosen && sourceData[i].product == productChosen) {
-            renderData.push(sourceData[i]);
+        let currentItem = sourceData[i];
+        let region = currentItem.region;
+        let product = currentItem.product;
+        for (let r = 0; r < regionSelected.length; r++) {
+            let currentRegion = regionSelected[r];
+            if (currentRegion == region) {
+                for (let p = 0; p < productSelected.length; p++) {
+                    let currentProduct = productSelected[p];
+                    if (currentProduct == product) {
+                        renderData.push(currentItem);
+                    }
+                }
+            }
         }
     }
     formatNewHTML(renderData);
@@ -62,6 +136,7 @@ function formatTableHeader() {
     tableHeader.appendChild(headerContent3);
 
     tableHeader.style.display = "flex";
+    tableHeader.style.marginBottom = "30px";
     headerContent1.style.width = "100px";
     headerContent2.style.width = "100px";
     headerContent3.style.display = "flex";
