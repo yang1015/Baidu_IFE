75f13899257f69aa424209d3392dc9ba22e7cfc6
yang1015 <itsak1992@gmail.com>
2018-08-12 18:38:34 +0800

    完成任务函数

--- CourseTasks/Academy1/StarryNight/app.js
+++ CourseTasks/Academy1/StarryNight/app.js
@@ -32,3 +32,58 @@ function changeDivSize() {
     document.getElementsByTagName('p')[1].textContent = "div 长: " + innerWidth / 2 + "px" + " 宽: " + innerHeight / 2 + "px"
 }
 
+function findDom() {
+    getAllListItem();
+    findAllHtmlSpanInOneSection('news-top');
+    findListItem('news-top', 'CSS');
+    getActiveLinkContent('news-top')
+}
+
+function getAllListItem() {
+    // 返回页面中所有li标签
+    let allLiElement = document.querySelectorAll('li');
+
+}
+
+function findAllHtmlSpanInOneSection(sectionId) {
+    // 返回某个section下所有span中内容为HTML的span标签
+    let sectionEl = document.getElementById(sectionId).getElementsByTagName('span');
+    // let sectionElChildNodes = sectionEl.childNodes; 搜了span 就已经是一个数组了 没有childNodes
+    // let spanList = sectionElChildNodes.getElementsByTagName('span'); 错误用法
+
+    // 要找的node可能不在section的第一层子元素里诶
+    for (let i = 0; i < sectionEl.length; i++) {
+        let currentChild = sectionEl[i];
+        if (currentChild.innerHTML == "HTML") {
+            console.log(currentChild);
+        }
+    }
+
+}
+
+function findListItem(sectionId, spanCont) {
+    // 返回某个section下，所有所包含span内容为spanCont的LI标签
+    let sectionEl = document.getElementById(sectionId).getElementsByTagName('span');
+    for (let i = 0; i < sectionEl.length; i++) {
+        let currentChild = sectionEl[i];
+        if (currentChild.innerHTML == spanCont && currentChild.parentNode.tagName == "LI" ) {
+            console.log(currentChild.parentNode);
+        }
+    }
+
+}
+
+function getActiveLinkContent(sectionId) {
+    // 返回某个section下，class为active的链接中包含的文字内容
+    let arr = [];
+    let sectionEl = document.getElementById(sectionId).getElementsByTagName('a');
+    //let sectionElChildNodes = sectionEl.childNodes;
+    for (let i = 0; i < sectionEl.length; i++) {
+        let currentChild = sectionEl[i];
+        if (currentChild.className == "active") {
+            console.log(currentChild);
+        }
+    }
+}
+
+
